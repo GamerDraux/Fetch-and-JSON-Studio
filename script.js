dabd7760404702357a03494064ccec1c1e021503
gamerDraux <61572578+GamerDraux@users.noreply.github.com>
2020-03-18 19:11:04 -0500

    almost all the way through the studio, got the active astronaut tags to turn to green, but had to use procedurally generated class tags and CSS.  Not sure that's what they wanted.

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
 window.addEventListener('load', function(){
     console.log ('window loaded');
+    let response = fetch("https://handlers.education.launchcode.org/static/astronauts.json");
+    response.then(function(response){
+        let json=response.json();
+        json.then(function(json){
+            json=json.sort(function(a,b){
+                return a.hoursInSpace- b.hoursInSpace;
+            });
+            json=json.reverse();
+            for (i=0; i<json.length; i++){
+                let container= document.getElementById('container');
+                container.innerHTML=container.innerHTML+(`
+                    <div class="astronaut">
+                        <div class="bio">
+                        <h3>${json[i].firstName} ${json[i].lastName}</h3>
+                        <ul>
+                            <li>Hours in space: ${json[i].hoursInSpace}</li>
+                            <li class="active${json[i].active}">Active: ${json[i].active}</li>
+                            <li>Skills: ${json[i].skills.join(', ')}</li>
+                        </ul>
+                    </div>
+                    <img class="avatar" src="${json[i].picture}">
+                    </div>
+                `);
+            }
+        })
+    })
 })
